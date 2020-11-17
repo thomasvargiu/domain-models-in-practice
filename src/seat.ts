1a2bcf9bc529e1aaf2258ff89a2da5ac081ab321
Thomas Vargiu <tvargiu@gmail.com>
2020-11-17 13:06:58 +0100

    Day 3 changes

--- src/seat.ts
+++ src/seat.ts
@@ -37,6 +37,15 @@ export class CustomerId {
     this.id = id
   }
 }
+export class Screen {
+    readonly screenId: ScreenId;
+    readonly startTime: Date;
+
+    constructor(screenId: ScreenId, startTime: Date) {
+        this.screenId = screenId;
+        this.startTime = startTime
+    }
+}
 export class ScreenId {
   private readonly id: string
 
@@ -62,7 +71,7 @@ export class ReserveSeat {
 export class EventStore {
   events: Object[]
 
-  constructor(events: Object[]) {
+  constructor(events: Object[] = []) {
     this.events = events
   }
 
@@ -71,13 +80,26 @@ export class EventStore {
   }
 }
 
+export class ScreenRepository {
+    readonly screen: Screen;
+    constructor(screen: Screen) {
+        this.screen = screen;
+    }
+    
+    find(id: ScreenId) {
+        return this.screen;
+    }
+}
+
 // Command Handler
 export class ReserveSeatHandler {
   private eventStore: EventStore
+  private screenRepository: ScreenRepository;
   private publish: (event: Object) => void
 
-  constructor(eventStore: EventStore, publish: (event: Object) => void) {
+  constructor(eventStore: EventStore, screenRepository: ScreenRepository, publish: (event: Object) => void) {
     this.eventStore = eventStore
+    this.screenRepository = screenRepository
     this.publish = publish
   }
 
@@ -85,9 +107,10 @@ export class ReserveSeatHandler {
     const customerId = new CustomerId(reserveSeat.customerId)
     const screenId = new ScreenId(reserveSeat.screenId)
     const seat = new Seat(reserveSeat.row, reserveSeat.col)
+    const screen = this.screenRepository.find(screenId)
 
     const events = this.eventStore.byScreenId(screenId)
-    const reservationState = new ReservationState(events)
+    const reservationState = new ReservationState(events, screen)
 
     const reservation = new Reservation(reservationState, this.publish)
 
@@ -125,19 +148,17 @@ export class SeatReservationRefused implements DomainEvent {
 
 export class ReservationState {
   reservedSeat: Seat[] = []
+  screen: Screen
 
-  constructor(events: Object[]) {
+  constructor(events: Object[], screen: Screen) {
+    this.screen = screen
     for (const event of events) {
-      this.apply(event as SeatReserved)
+        this.apply(event as SeatReserved)
     }
   }
 
   apply(event: SeatReserved) {
     this.reservedSeat.push(event.seat)
-  }
-
-  isAvailable(seat: Seat) {
-    return !this.reservedSeat.find((s) => s.equals(seat))
   }
 }
 
@@ -151,8 +172,20 @@ export class Reservation {
     this.publish = publish
   }
 
+    isOnTime() {
+        return this.reservationState.screen.startTime > new Date((new Date()).getTime() - (15 * 60 * 1000));
+    }
+
+    isAvailable(seat: Seat) {
+        return !this.reservationState.reservedSeat.find((s) => s.equals(seat))
+    }
+
+    canBook(seat: Seat) {
+        return this.isOnTime() && this.isAvailable(seat);
+    }
+
   reserveSeat(customerId: CustomerId, screenId: ScreenId, seat: Seat) {
-    if (this.reservationState.isAvailable(seat)) this.publish(new SeatReserved(customerId, screenId, seat))
+    if (this.canBook(seat)) this.publish(new SeatReserved(customerId, screenId, seat))
     else this.publish(new SeatReservationRefused(customerId, screenId, seat))
   }
 }
--- test/examples/booking_handler.test.ts
+++ test/examples/booking_handler.test.ts
@@ -11,13 +11,42 @@ import {
   ScreenId,
   SeatReservationRefused,
   DomainEvent,
+  ScreenRepository,
+  Screen,
 } from "../../src/seat"
+
+/*
+class TestFramework {
+    private history: EventStore = new EventStore()
+    private publishedEvents: DomainEvent[] = [];
+
+    given(events: EventStore) {
+        this.history = events;
+    }
+
+    when(command: Object) {
+        const screenRepository = new ScreenRepository();
+        const handler = new ReserveSeatHandler(
+            this.history,
+            new ScreenRepository(),
+            function (event: Object) {
+
+            }
+        );
+    }
+}
+*/
+
 
 describe("A Customer reserves specific seats at a specific screening (for simplicity, assume there exists only one screening for the time beeing)", () => {
   it("If available, the seats should be reserved.", async () => {
     const events: DomainEvent[] = []
     const eventStore = new EventStore([])
-    const commandHandler = new ReserveSeatHandler(eventStore, (e) => events.push(e))
+      const screenId = new ScreenId("screen1")
+      const screenStartTime = new Date((new Date()).getTime() + (20 * 60 * 1000));
+      const screen = new Screen(screenId, screenStartTime);
+      const screenRepository = new ScreenRepository(screen);
+      const commandHandler = new ReserveSeatHandler(eventStore, screenRepository, (e) => events.push(e))
     const command = new ReserveSeat("customer1", "screen1", Row.A, Col.ONE)
 
     commandHandler.handleCommand(command)
@@ -30,12 +59,32 @@ describe("A Customer reserves specific seats at a specific screening (for simpli
     const eventStore = new EventStore([
       new SeatReserved(new CustomerId("customer1"), new ScreenId("screen1"), new Seat(Row.A, Col.ONE)),
     ])
-    const commandHandler = new ReserveSeatHandler(eventStore, (e) => events.push(e))
+    const screenId = new ScreenId("screen1")
+      const screenStartTime = new Date((new Date()).getTime() + (20 * 60 * 1000));
+      const screen = new Screen(screenId, screenStartTime);
+      const screenRepository = new ScreenRepository(screen);
+    const commandHandler = new ReserveSeatHandler(eventStore, screenRepository, (e) => events.push(e))
     const command = new ReserveSeat("customer2", "screen1", Row.A, Col.ONE)
 
     commandHandler.handleCommand(command)
 
     expect(events.length).eql(1)
     expect(events[0]).to.be.instanceOf(SeatReservationRefused)
+  })
+
+  it("If not available, but too late.", async () => {
+    const events: DomainEvent[] = []
+    const eventStore = new EventStore([])
+      const screenId = new ScreenId("screen1")
+      const screenStartTime = new Date((new Date()).getTime() + (14 * 60 * 1000));
+      const screen = new Screen(screenId, screenStartTime);
+      const screenRepository = new ScreenRepository(screen);
+    const commandHandler = new ReserveSeatHandler(eventStore, screenRepository, (e) => events.push(e))
+    const command = new ReserveSeat("customer2", "screen1", Row.A, Col.ONE)
+
+    commandHandler.handleCommand(command)
+
+    expect(events.length).eql(1)
+      expect(events[0]).to.be.instanceOf(SeatReservationRefused)
   })
 })
